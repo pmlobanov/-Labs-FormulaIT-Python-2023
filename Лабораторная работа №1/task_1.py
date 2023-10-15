8108a824e86ba8d060daac9699749fd9f7154c26
pmlobanov <147930157+pmlobanov@users.noreply.github.com>
2023-10-15 22:20:21 +0300

    Update task_1.py

--- Лабораторная работа №1/task_1.py
+++ Лабораторная работа №1/task_1.py
@@ -1,5 +1,7 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
-numbers[4] = 0
-numbers[4] = sum(numbers)/len(numbers)
+
+missing_index = numbers.index(None)
+numbers[missing_index] = (sum(numbers[:missing_index:]) + sum(numbers[(missing_index+1)::]))\
+                         / len(numbers)
 print("Измененный список:", numbers)
